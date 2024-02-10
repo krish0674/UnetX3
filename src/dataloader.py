03e96322e7f963bd6c8283d63c75b3e96a403ea0
krish0674 <118186942+krish0674@users.noreply.github.com>
2024-02-10 19:45:06 +0530

    Update dataloader.py

--- src/dataloader.py
+++ src/dataloader.py
@@ -26,8 +26,8 @@ class Dataset(BaseDataset):
     def __getitem__(self, i):
         
         # read data
-        himage = cv2.imread(self.hr_list[i], cv2.IMREAD_GRAYSCALE)
-        target = cv2.imread(self.tar_list[i], cv2.IMREAD_GRAYSCALE)
+        himage = cv2.imread(self.hr_list[i], 0)
+        target = cv2.imread(self.tar_list[i], 0)
         
         #timage = cv2.imread(self.thermal_list[i])
         # apply augmentations
