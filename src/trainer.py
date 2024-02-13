8d6e202cd39777d095cf8f72b1c02b84240b9e11
krish0674 <118186942+krish0674@users.noreply.github.com>
2024-02-13 19:34:54 +0530

    Update trainer.py

--- src/trainer.py
+++ src/trainer.py
@@ -29,8 +29,8 @@ def train(epochs, batch_size, hr_dir, tar_dir, hr_val_dir, tar_val_dir, encoder=
     def get_transform():
         return A.Compose([
             A.Resize(448, 640),
-            A.HorizontalFlip(p=0.3),
-            A.VerticalFlip(p=0.3),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.5),
             ToTensorV2(),
     ])
 
