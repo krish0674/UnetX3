fcb928f6f44420811593b2b0366c6f0b36ea331d
krish0674 <118186942+krish0674@users.noreply.github.com>
2024-02-13 20:27:12 +0530

    Update transformations.py

--- src/transformations.py
+++ src/transformations.py
@@ -2,50 +2,16 @@ import albumentations as albu
 import numpy as np
 def get_training_augmentation():
     train_transform = [
-        albu.Resize(480,640,always_apply=True),
+        albu.Resize(448,640,always_apply=True),
         albu.HorizontalFlip(p=0.5),
         albu.VerticalFlip(p=0.5),
-
-#         albu.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=0.5, border_mode=0),
-        
-#         albu.RandomCrop(height=512, width=512),
-#         albu.ColorJitter(p=0.5),
-#         albu.IAAAdditiveGaussianNoise(p=0.2),
-#         albu.IAAPerspective(p=0.5),
-        albu.PadIfNeeded(min_height=480, min_width=640, always_apply=True, border_mode=0),
-#         albu.OneOf(
-#             [
-#                 albu.CLAHE(p=1),
-#                 albu.RandomBrightness(p=1),
-#                 albu.RandomGamma(p=1),
-#             ],
-#             p=0.9,
-#         ),
-
-#         albu.OneOf(
-#             [
-#                 albu.IAASharpen(p=1),
-#                 albu.Blur(blur_limit=3, p=1),
-#                 albu.MotionBlur(blur_limit=3, p=1),
-#             ],
-#             p=0.9,
-#         ),
-
-#         albu.OneOf(
-#             [
-#                 albu.RandomContrast(p=1),
-#                 albu.HueSaturationValue(p=1),
-#             ],
-#             p=0.9,
-#         ),
     ]
     return albu.Compose(train_transform,additional_targets={'image1':'mask'})
     
 def get_validation_augmentation():
     test_transform = [
-        albu.Resize(480,640,always_apply=True),
-        albu.PadIfNeeded(480,640)
-    ]
+        albu.Resize(448,640,always_apply=True),
+]
     return albu.Compose(test_transform,additional_targets={'image1':'mask'})
 
 
