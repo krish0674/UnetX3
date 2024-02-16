0f53c54432d3bb45210b207bfb80bd2d7ff402e7
krish0674 <118186942+krish0674@users.noreply.github.com>
2024-02-16 17:45:01 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -206,23 +206,7 @@ def discriminator_block(in_filters, out_filters, normalization=False):
         layers.append(nn.InstanceNorm2d(out_filters, affine=True))
     return layers
 
-class Discriminator(nn.Module):
-    def __init__(self):
-        super(Discriminator, self).__init__()
 
-        self.model = nn.Sequential(
-            nn.Conv2d(1, 16, 3, stride=2, padding=1),
-            nn.LeakyReLU(0.2),
-            nn.InstanceNorm2d(16, affine=True),
-            *discriminator_block(16, 32),
-            *discriminator_block(32, 64),
-            *discriminator_block(64, 128),
-            *discriminator_block(128, 128),
-            nn.Conv2d(128, 1, 8, padding=0)
-        )
-
-    def forward(self, img_input):
-        return self.model(img_input)
             
 
             
