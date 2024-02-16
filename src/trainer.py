ac996777f50c86d40b5427686fd157edf12171a5
krish0674 <118186942+krish0674@users.noreply.github.com>
2024-02-16 17:56:06 +0530

    Update trainer.py

--- src/trainer.py
+++ src/trainer.py
@@ -4,7 +4,7 @@ from .train_utils import TrainEpoch, ValidEpoch
 from .loss import custom_loss, custom_lossv,lossX3_mse,lossX3_p
 from .dataloader import Dataset #,list_image_paths
 from .transformations import get_training_augmentation, get_validation_augmentation, get_preprocessing
-from .model import UnetX3
+from .model import UnetX3,Discriminator
 from torchmetrics import StructuralSimilarityIndexMeasure
 from torchmetrics import PeakSignalNoiseRatio
 import torch
@@ -16,7 +16,7 @@ import numpy as np
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from torch.optim.lr_scheduler import ExponentialLR
-
+from torch import nn
 #from torch.utils.data import Dataset, DataLoader
 
 def train(epochs, batch_size, hr_dir, tar_dir, hr_val_dir, tar_val_dir, encoder='resnet34', encoder_weights='imagenet', device='cuda', lr=1e-4):
@@ -27,6 +27,7 @@ def train(epochs, batch_size, hr_dir, tar_dir, hr_val_dir, tar_val_dir, encoder=
         encoder_name=encoder, 
         encoder_weights=encoder_weights, 
     )
+    discriminator=Discriminator()
 
     def get_transform():
         return A.Compose([
@@ -50,63 +51,73 @@ def train(epochs, batch_size, hr_dir, tar_dir, hr_val_dir, tar_val_dir, encoder=
     transform=transform
     )
 
-    # Create the DataLoader
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    valid_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+    valid_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
-    loss=lossX3_mse()
+    loss = lossX3_mse()
+    d_loss_fn = nn.BCELoss()  
+    g_loss_fn = nn.BCELoss()
 
     Z = StructuralSimilarityIndexMeasure()
     P = PeakSignalNoiseRatio()
     P.__name__ = 'psnr'
     Z.__name__ = 'ssim'
-    metrics = [
-        Z,
-        P,
-    ]
+    metrics = [Z, P]
 
-    optimizer = torch.optim.Adam([ 
-        dict(params=model.parameters(), lr=lr),
-    ])
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    d_optimizer = torch.optim.Adam(discriminator.parameters(), lr=lr * 0.1)  # Discriminator optimizer, consider using a different learning rate
 
     scheduler = ExponentialLR(optimizer, gamma=0.96)
 
     train_epoch = TrainEpoch(
-        model, 
-        loss=loss, 
-        metrics=metrics, 
-        optimizer=optimizer,
+        model=model,
+        discriminator=discriminator,
+        loss=loss,
+        metrics=metrics,
+        g_optimizer=optimizer,
+        d_optimizer=d_optimizer,
+        g_loss_fn=g_loss_fn,
+        d_loss_fn=d_loss_fn,
         device=device,
         verbose=True,
     )
+
     valid_epoch = ValidEpoch(
-        model, 
-        loss=loss, 
-        metrics=metrics, 
+        model=model,
+        discriminator=discriminator,
+        loss=loss,
+        metrics=metrics,
+        g_loss_fn=g_loss_fn,
         device=device,
         verbose=True,
     )
 
     max_ssim = 0
     max_psnr = 0
-    counter = 0
     for i in range(0, epochs):
-        
         print('\nEpoch: {}'.format(i))
         train_logs = train_epoch.run(train_loader)
         valid_logs = valid_epoch.run(valid_loader)
         scheduler.step()
+
         print(train_logs)
-        wandb.log({'epoch':i+1,'t_loss':train_logs['loss_x3_mse'],'t_ssim':train_logs['ssim'],'v_loss':valid_logs['loss_x3_mse'],'v_ssim':valid_logs['ssim'],'v_psnr':valid_logs['psnr'],'t_psnr':train_logs['psnr']})
+        wandb.log({
+            'epoch': i+1,
+            't_loss': train_logs['loss_x3_mse'],
+            't_ssim': train_logs['ssim'],
+            'v_loss': valid_logs['loss_x3_mse'],
+            'v_ssim': valid_logs['ssim'],
+            'v_psnr': valid_logs['psnr'],
+            't_psnr': train_logs['psnr']
+        })
+
+        # Save the model if validation SSIM improves
         if max_ssim <= valid_logs['ssim']:
             max_ssim = valid_logs['ssim']
             max_psnr = valid_logs['psnr']
-            wandb.config.update({'max_ssim':max_ssim, 'max_psnr':max_psnr}, allow_val_change=True)
+            wandb.config.update({'max_ssim': max_ssim, 'max_psnr': max_psnr}, allow_val_change=True)
             torch.save(model.state_dict(), './best_model.pth')
             print('Model saved!')
-            counter = 0
-        counter = counter+1
-    print(f'max ssim: {max_ssim} max psnr: {max_psnr}')
 
 def train_model(configs):
     train(configs['epochs'], configs['batch_size'], configs['hr_dir'], configs['tar_dir'],
