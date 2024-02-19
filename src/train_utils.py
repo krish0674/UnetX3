71baae2871d0e3698e000d81a9630ca0a0eca2d5
krish0674 <118186942+krish0674@users.noreply.github.com>
2024-02-19 20:25:37 +0530

    Update train_utils.py

--- src/train_utils.py
+++ src/train_utils.py
@@ -187,9 +187,8 @@ class TrainEpoch(Epoch):
         l_g_total += l_g_pix
 
         # gan loss
-        disc_output=self.discriminator(prediction_c).squeeze()
-        print(disc_output.shape)
-        disc_output=disc_output.unsqueeze(1)
+        disc_output=self.discriminator(prediction_c).squeeze(1).squeeze(1).squeeze(1)
+       
         print(disc_output.shape)
         g_loss_fake = self.g_loss_fn( disc_output,torch.ones(prediction_c.size(0), 1, device=self.device))
         l_g_total += g_loss_fake
@@ -205,13 +204,11 @@ class TrainEpoch(Epoch):
 
         self.d_optimizer.zero_grad()
         
-        disc_output=self.discriminator(y).squeeze()
-        disc_output=disc_output.unsqueeze(1)
+        disc_output=self.discriminator(y).squeeze(1).squeeze(1).squeeze(1)
 
         real_loss = self.d_loss_fn(disc_output ,torch.ones(y.size(0), 1, device=self.device))
         prediction_a, prediction_b, prediction_c = self.model(x) 
-        disc_output=self.discriminator(prediction_c).squeeze()
-        disc_output=disc_output.unsqueeze(1)
+        disc_output=self.discriminator(prediction_c).squeeze(1).squeeze(1).squeeze(1)
         fake_loss = self.d_loss_fn(disc_output, torch.zeros(prediction_c.size(0), 1, device=self.device))
         gradient_penalty = compute_gradient_penalty(self.discriminator, y, prediction_c, self.device)
         d_loss = real_loss + fake_loss + self.gp_weight * gradient_penalty
@@ -240,8 +237,7 @@ class ValidEpoch(Epoch):
             l_g_pix = self.loss(prediction_a, prediction_b, prediction_c, y)
 
             # gan loss
-            disc_output=self.discriminator(prediction_c).squeeze()
-            disc_output=disc_output.unsqueeze(1)
+            disc_output=self.discriminator(prediction_c).squeeze(1).squeeze(1).squeeze(1)
             g_loss_fake = self.g_loss_fn(disc_output, torch.ones(prediction_c.size(0), 1, device=self.device))
 
             # Total loss is the sum of pixel loss and GAN loss, without averaging them since we are not optimizing the discriminator here
