878b40050b43c221b9637bd7a916e4f00605a619
krish0674 <118186942+krish0674@users.noreply.github.com>
2024-02-21 19:48:34 +0530

    Update train_utils.py

--- src/train_utils.py
+++ src/train_utils.py
@@ -205,12 +205,12 @@ class TrainEpoch(Epoch):
         disc_output = torch.sigmoid(disc_output)  
         real_loss = self.d_loss_fn(disc_output ,torch.ones(y.size(0), 1, device=self.device))
         # prediction_a, prediction_b, prediction_c = self.model(x) 
-
+        with torch.no_grad():  # Ensure no gradients are accumulated for the generator
+            prediction_a, prediction_b, prediction_c = self.model(x) 
         disc_output=self.discriminator(prediction_c).squeeze(1).squeeze(1)
         disc_output = torch.sigmoid(disc_output)  
         fake_loss = self.d_loss_fn(disc_output, torch.zeros(prediction_c.size(0), 1, device=self.device))
-        prediction_c_detached = prediction_c.detach()
-        gradient_penalty = compute_gradient_penalty(self.discriminator, y, prediction_c_detached, self.device)        
+        gradient_penalty = compute_gradient_penalty(self.discriminator, y, prediction_c, self.device)
         d_loss = real_loss + fake_loss + self.gp_weight * gradient_penalty
 
         d_loss.backward()
